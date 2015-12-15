1154bcecd788ca5a9af0f9b7f408e9140c29ec02
nat <naesst@gmail.com>
2015-12-16 00:32:48 +0100

    Si en lugar de ',' hay '\n'
    Mejora por si en lugar de ‘,’ hay ‘\n’.

--- Calculadora/calculadora.php
+++ Calculadora/calculadora.php
@@ -1,12 +1,22 @@
 <?php
 
-$resultado = sumar("");
+$resultado = sumar("2\n3");
 
 echo "El resultado de la suma es: " . $resultado;
 
 function sumar($arg1 = 0){
     $suma = 0;
-    $valores = explode(",",$arg1);
+    
+    $pos = strpos($arg1, ",");
+    if ($pos){
+        $simbolo = ",";
+    }
+    else{
+        $simbolo = "\n";
+    }
+        
+    $valores = explode($simbolo,$arg1);
+
     for ($i=0; $i < sizeof($valores); $i++){
         $suma = $suma + $valores[$i];
     }
